9fa4c5a5c1dfc38aa7337492acc3264471ea8229
Jesse <boatengjesse921@gmail.com>
2025-05-22 14:58:54 +0000

    Test 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -32,7 +32,7 @@ export default function HomeScreen() {
               web: 'F12',
             })}
           </ThemedText>{' '}
-          to open developer tos
+          to open developer tools.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
